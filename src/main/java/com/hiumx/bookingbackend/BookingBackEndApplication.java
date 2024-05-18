8dcbb862827a67ad3a9f9694b34e2cf8d130d451
hiumx <maixuanhieu250103@gmail.com>
2024-05-18 13:00:01 +0700

    [INIT] Initial backend of booking project

---
+++ src/main/java/com/hiumx/bookingbackend/BookingBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.hiumx.bookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingBackEndApplication.class, args);
+	}
+
+}
